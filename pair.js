3f95af8ed6f784a1024e01620557c6c76c1f0eba
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-10-01 13:56:36 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -29,7 +29,7 @@ async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
             email: 'agni119.67@gmail.com', // Your Mega A/c Email Here
-            password: 'Shashika@2008' // Your Mega A/c Password Here
+            password: 'shashika@2008' // Your Mega A/c Password Here
         }).ready;
         console.log('Mega storage initialized.');
 
